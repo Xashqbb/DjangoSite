3e0d33f8568fea7506c37fb01878028364dd1836
Xashqbb <mrhoriizonn@gmail.com>
2023-08-09 20:19:57 +0300

    finish cart and checkout

--- furnituresite/cart/views.py
+++ furnituresite/cart/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render,redirect
 from .models import *
-from furniturestore.models import FurnitureProduct
+
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0,'get_cart_items':0}
+
+    context = {'items':items,'order':order}
     return render(request,'cart/cart.html',context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
     return render(request,'cart/checkout.html',context)
 
