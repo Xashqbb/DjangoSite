b2b6039eb7c8df38a7f17993ba0834ea5c9722ae
Xashqbb <mrhoriizonn@gmail.com>
2023-08-22 16:53:54 +0300

    save

--- furnituresite/furniturestore/models.py
+++ furnituresite/furniturestore/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,null=True)
 
     def __str__(self):
         return self.name
