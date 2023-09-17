f92b74462030d586db4a4b814325d33992d11989
Xashqbb <mrhoriizonn@gmail.com>
2023-09-17 19:55:37 +0300

    list of products(category)

--- furnituresite/furniturestore/urls.py
+++ furnituresite/furniturestore/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path,include
+from django.urls import path, include
 from .views import *
 
-
 urlpatterns = [
-    path('',furniture_store,name='furniture_store'),
-    path('<slug:product_slug>/',product_detail,name='product_detail'),
+    path('', furniture_store, name='furniture_store'),
+    path('<slug:product_slug>/', product_detail, name='product_detail'),
+    path('category/<slug:category_slug>/', furniture_store, name='furniture_store_category'),
+    path('category/all/', furniture_store, name='furniture_store_all'),
 ]
--- furnituresite/furniturestore/views.py
+++ furnituresite/furniturestore/views.py
@@ -3,21 +3,31 @@ from cart.models import *
 from .models import *
 from utils import cookieCart,cartData
 
-def furniture_store(request):
-
+def furniture_store(request, category_slug=None):
     data = cartData(request)
     cartItems = data['cartItems']
-    order = data['order']
-    items = data['items']
+    categories = Category.objects.all()
 
-    products = FurnitureProduct.objects.all()
-    context = {'products':products,'cartItems':cartItems}
-    return render(request,'furniturestore/furniture_store.html',context)
+    if category_slug:
+        if category_slug == 'all':
+            products = FurnitureProduct.objects.all()
+        else:
+            products = FurnitureProduct.objects.filter(category__slug=category_slug)
+    else:
+        products = FurnitureProduct.objects.all()
+
+    context = {
+        'products': products,
+        'cartItems': cartItems,
+        'categories': categories,
+        'current_category_slug': category_slug,
+    }
+    return render(request, 'furniturestore/furniture_store.html', context)
 
 
 def product_detail(request, product_slug):
     data = cartData(request)
     cartItems = data['cartItems']
     product = get_object_or_404(FurnitureProduct, slug=product_slug)
-    context = {'product': product,'cartItems':cartItems}
+    context = {'product': product, 'cartItems': cartItems}
     return render(request, 'furniturestore/product_detail.html', context)
