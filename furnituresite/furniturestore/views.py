a9fa2496bc10dc1dbfb76351edddc025962d0e3a
Xashqbb <mrhoriizonn@gmail.com>
2023-09-26 20:34:57 +0300

    list of products(category)

--- furnituresite/furniturestore/views.py
+++ furnituresite/furniturestore/views.py
@@ -15,12 +15,12 @@ def furniture_store(request, category_slug=None):
             products = FurnitureProduct.objects.filter(category__slug=category_slug)
     else:
         products = FurnitureProduct.objects.all()
-
     context = {
         'products': products,
         'cartItems': cartItems,
         'categories': categories,
         'current_category_slug': category_slug,
+
     }
     return render(request, 'furniturestore/furniture_store.html', context)
 
@@ -29,5 +29,5 @@ def product_detail(request, product_slug):
     data = cartData(request)
     cartItems = data['cartItems']
     product = get_object_or_404(FurnitureProduct, slug=product_slug)
-    context = {'product': product, 'cartItems': cartItems}
+    context = {'product': product, 'cartItems': cartItems,}
     return render(request, 'furniturestore/product_detail.html', context)
