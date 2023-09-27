0b5d043ab78cefbf91fb966272b9280b8b8f4885
Xashqbb <mrhoriizonn@gmail.com>
2023-09-27 14:49:01 +0300

    pagination

--- furnituresite/furniturestore/views.py
+++ furnituresite/furniturestore/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from cart.models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import *
 from utils import cookieCart,cartData
 
@@ -15,14 +16,32 @@ def furniture_store(request, category_slug=None):
             products = FurnitureProduct.objects.filter(category__slug=category_slug)
     else:
         products = FurnitureProduct.objects.all()
+
+    paginator = Paginator(products, 10)
+    page = request.GET.get('page')
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    page_number = products.number
+    num_pages = paginator.num_pages
+    page_range = range(max(1, page_number - 2), min(num_pages, page_number + 2) + 1)
+
     context = {
         'products': products,
         'cartItems': cartItems,
         'categories': categories,
         'current_category_slug': category_slug,
-
+        'page_range': page_range,
+        'page_number': page_number,
+        'num_pages': num_pages,
     }
     return render(request, 'furniturestore/furniture_store.html', context)
+
 
 
 def product_detail(request, product_slug):
